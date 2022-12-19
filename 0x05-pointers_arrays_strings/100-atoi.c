52c6d64375c1b5db2a1d9f834a91a8c4971e6ea6
Revelation-coder <82636655+Revelation-coder@users.noreply.github.com>
2022-12-19 05:30:15 -0800

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -5,13 +5,13 @@
  *
  * Return: Always 0.
  */
-char* str[10]{
+char* str(){
 return "122";
 }
-char* str2[10]{
+char* str2(){
 return "Hello!";
 }
-char* str[10]{
+char* str3(){
 return "99Hello!";
 }
 int main() {
@@ -20,17 +20,17 @@ int main() {
 	char str2[10];
 	char str3[10];
 	int x;
-    	char* str[10] = "122";
+    	char* str[10] = char* str();
   	x = atoi(str);
 	printf("Converting '122': %d\n", x);
 
 	/* Converting an alphanumeric string */
-	char* str2[10] = "Hello!";
+	char* str2[10] = char* str2();
   	x = atoi(str2);
         printf("Converting 'Hello!': %d\n", x);
 
         /* Converting a partial string */
-        char* str3[10] = "99Hello!";
+        char* str3[10] = char* str3();
         x = atoi(str3);
         printf("Converting '99Hello!': %d\n", x);
         return 0;
