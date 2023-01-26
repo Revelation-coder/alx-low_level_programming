6bf10fb6be3433fa5ebbe204af81b766775a5583
Revelation-coder <82636655+Revelation-coder@users.noreply.github.com>
2023-01-26 11:58:50 +0200

    Add files via upload

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,19 +1,17 @@
-#include <stdlib.h>
 #include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
 
 /**
  * free_list - frees a linked list
- * @head: list_t list to be freed
+ * @head: the beginning of the list
  */
+
 void free_list(list_t *head)
 {
-	list_t *tmp;
-
-	while (head)
-	{
-		tmp = head->next;
-		free(head->str);
-		free(head);
-		head = tmp;
-	}
+	if (head == NULL)
+		return;
+	free_list(head->next);
+	free(head->str);
+	free(head);
 }
