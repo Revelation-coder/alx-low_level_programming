918b01c45ae840dd7f1823ef2aeb45d0e4334ed7
Revelation-coder <82636655+Revelation-coder@users.noreply.github.com>
2023-01-26 11:50:36 +0200

    Add files via upload
    updated 3 and 4

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -15,11 +15,13 @@ int _strlen(const char *s)
 		;
 	return (i);
 }
+
 /**
  * _strdup - recreation of string duplicate function
  * @src: source of string to duplicate
  * Return: pointer to malloc'd space with copied string
  */
+
 void *_strdup(const char *src)
 {
 	int len, i;
@@ -34,12 +36,14 @@ void *_strdup(const char *src)
 	dest[i] = '\0';
 	return (dest);
 }
+
 /**
  * add_node_end - add new nodes to the end of the list
  * @head: current place in the list
  * @str: string to add to the head
  * Return: pointer to current position in list
  */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new, *current;
