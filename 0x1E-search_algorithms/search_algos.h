bab402213a789fc56925f7821cb0df9693221e77
Revelation-coder <captainrevelation@gmail.com>
2023-07-12 02:37:27 -0700

    functions

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -31,5 +31,6 @@ int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
 
 listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_search(skiplist_t *list, skiplist_t *express, int value)
 skiplist_t *linear_skip(skiplist_t *list, int value);
 #endif
